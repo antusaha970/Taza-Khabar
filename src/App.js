b18d2a601f783b63ac3e0abd2bc3045ead271c8f
antu <antusaha970@gmail.com>
2023-01-19 19:30:58 +0600

    Headlines box coded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './Components/Hero/Hero';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Headlines from './Components/Headlines/Headlines';
+import { useEffect, useState } from 'react';
+
+
+
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(()=>{
+    fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cd4133b3dc8c4ffa9d7e2a5e481bf69b')
+    .then(res => res.json())
+    .then(data => setNews(data.articles))
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hero></Hero>
       </header>
+      <main>
+      <Container fixed>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item md={8} xs={12}>
+              <h1>Headlines</h1>
+              <Grid container spacing={2}>
+                  {
+                    news?.map((article, index) => <Grid item md={6} xs={12}><Headlines article = {article}></Headlines></Grid>)
+                  }
+              </Grid>
+            </Grid>
+            <Grid item md={4} xs={12}>
+              <h3>News</h3>
+            </Grid>
+          </Grid>
+        </Box>
+        </Container>
+      </main>
     </div>
   );
 }
---
+++ src/Components/Headlines/Headlines.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function Headlines(props) {
+    const { title, description, url, urlToImage } = props.article;
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image={urlToImage}
+                title="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {description}
+
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small"><a href={url} target="_blank">Read More</a></Button>
+            </CardActions>
+        </Card>
+    );
+}
